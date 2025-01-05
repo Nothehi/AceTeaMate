f45fd57953fa45325aaec1f253b1196507c3bcaf
nothehi <nodehi.work@gmail.com>
2025-01-05 22:55:22 +0330

    wip: estimation page

---
+++ src/views/EstimateView.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <main
+        class="max-h-screen max-w-screen h-screen w-screen bg-gradient-to-br from-[#374966] to-[#2d3b58] flex flex-col justify-start items-center">
+        <h1 class="my-2 font-bold text-white">Hello, World</h1>
+        <div class="bg-gradient-to-b from-[#55709a] to-[#334260] w-4/5 h-5/6 shadow-2xl rounded-[5.5rem] p-1">
+            <div class="bg-gradient-to-b from-[#4d678a] to-[#344566] w-full h-full shadow-lg rounded-[5.25rem] p-4">
+                <div
+                    class="bg-gradient-to-b from-[#314666] to-[#314667] w-full h-full shadow-inner shadow-[#455c7e] rounded-[4.5rem]">
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full flex flex-row items-center justify-center mt-6">
+            <button class="bg-[#9ad360]">
+                +
+            </button>
+        </div>
+    </main>
+</template>
